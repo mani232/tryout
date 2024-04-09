d834022bda896a33e625ba0321eceeaf84a438e7
Manibalan V <manibalan.v@zilogic.com>
2024-04-09 16:47:06 +0530

    .

--- Makefile
+++
@@ -1,9 +0,0 @@
-SRC := hello.c
-CC ?= gcc
-OBJ := hello
-
-install: $(OBJ)
-	$(CC) $(SRC) -o $(OBJ)
-
-clean:
-	rm $(OBJ)
--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-  printf("Hello world\n");
-  printf("Hi Mani\n");
-  return 0;
-}
